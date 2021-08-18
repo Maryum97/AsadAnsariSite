87a602d3d8c8571cab6a7cdbfd9ce5f485fa2522
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-18 16:51:17 +0100

    added navbar and installed dependencies

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'About Me',
+        url: '#about',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Portfolio',
+        url: '#portfolio',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Contact Me',
+        url: '#contact',
+        cName: 'nav-link'
+    }
+]
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
+// import components here
+import Navbar from '../components/Navbar';
+
 function MainPage() {
     return (
         <div>
+            <Navbar />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
+            <h1>Hi, this is the main page!</h1>
             <h1>Hi, this is the main page!</h1>
         </div>
     )
