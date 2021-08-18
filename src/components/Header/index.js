b64e273e1c281ffc941784d6138edf751ca74162
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-18 19:24:44 +0100

    added aos animations

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.css';
 
+// aos dependencies
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 function Header(props) {
+    // initialise aos
+    useEffect(() => {
+        Aos.init({ duration: 600 })
+    }, []);
+
     return (
         <div className='section-header'>
-            <h1 className='heading'>{props.heading}</h1>
+            <h1
+                className='heading'
+                data-aos='slide-left'
+                data-aos-once="false"
+                data-aos-delay="100"
+            >{props.heading}</h1>
         </div>
     )
 }
