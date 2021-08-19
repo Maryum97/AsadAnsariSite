439bbd3652bd61bccbcf1957c8468bceb21f6dac
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-19 16:39:50 +0100

    added new files for philanthropy content

---
+++ src/components/Philanthropy/PhilItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PhilItems() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PhilItems;
---
+++ src/components/Philanthropy/PhilQuotation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PhilQuotation() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PhilQuotation;
