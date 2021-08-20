276b553db66f8b1468e4aa2eeb62da956198284b
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-20 01:13:08 +0100

    added text and applied aos

--- src/components/Philanthropy/PhilQuotation.js
+++ src/components/Philanthropy/PhilQuotation.js
@@ -17,10 +17,31 @@ function PhilQuotation() {
         <div className='quotation-container'>
             <div className='quotation-grid'>
                 <div className='quot-img'>
-                    <img src={QuotImg} className='img-fluid'></img>
+                    <img
+                        src={QuotImg}
+                        className='img-fluid quot-image'
+                        data-aos='fade-in'
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                        data-aos-once="false"
+                        data-aos-delay='200'
+                    ></img>
                 </div>
-                <div className='quot-text'>
-
+                <div
+                    className='quot-text'
+                    data-aos='zoom-in'
+                    data-aos-easing="ease-in-out"
+                    data-aos-mirror="true"
+                    data-aos-once="false"
+                    data-aos-delay='300'
+                >
+                    <p className='quotation'>
+                        “Philanthropy is a huge part of my life. It fills a gap for me. The more I help others, the more it pays off personally. In a small way, I’m trying to help make a lasting impact in others’ lives. All the initiatives I’m working on with trustable charities in different sectors, help me do that."
+                    </p>
+                    <br></br>
+                    <p className='quotation'>
+                        ~ Asad Ansari
+                    </p>
                 </div>
             </div>
         </div>
